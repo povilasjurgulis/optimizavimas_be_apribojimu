689de6d90d80dc5c457207560caadd34a6604dda
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2026-03-31 20:18:18 +0300

    Parašiau pirmą versiją (neužbaigtą ir neveikiančią) gradientinio nusileidimo algoritmo, kad matytusi idėja

--- grad_nusileidimas.py
+++ grad_nusileidimas.py
@@ -1,4 +1,15 @@
 # Gradientinio nusileidimo algoritmas
+import numpy as np
 
-def grad_fun(f, gradF, x0, e, Nmax):
-    f
+# gamma - žingsnio daugiklis; 
+def grad_fun(f, gradF, x0, gamma, eps, Nmax):
+    x = x0
+    k = 0
+
+    while k < Nmax:
+        g = gradF(x)
+        if np.linalg.norm(g) < eps: # jeigu gradiento norma mažesnė nei epsilon
+            break
+        x = x - gamma * g
+
+    return x
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import grad_nusileidimas
+
+def gradF(x):
+    return x*x
